721e3c5489cb96fd95d95867869ead8b0628fce3
kwhit2 <kfw2@outlook.com>
2021-07-26 15:27:28 +0000

    Bones Upload

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* insert_node - inserts a number into a sorted singly linked list.
+* @number: int
+* @head: first node
+* Return: the address of the new node, or NULL if it failed
+*/
+
+listint_t *insert_node(listint_t **head, int number);
+{
+    
+}
