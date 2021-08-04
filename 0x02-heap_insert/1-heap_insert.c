7385b514c7866760dae6169727e66f43b6f83cf0
kwhit2 <kfw2@outlook.com>
2021-08-04 15:56:46 +0000

    Seg faulting currently

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+* heap_insert - function that inserts a value into a Max Binary Heap
+* @root: is a double pointer to the root node of the Heap
+* @value: is the value to store in the node to be inserted
+*
+* Return: a pointer to the inserted node, or NULL on failure
+*/
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	*root = *root;
+	value = value;
+
+	return (*root);
+}
