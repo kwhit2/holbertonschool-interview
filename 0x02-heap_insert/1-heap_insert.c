28e44684a94cb7e542b21eea31cfdaf7d361241d
kwhit2 <kfw2@outlook.com>
2021-08-05 16:08:05 +0000

    Stil seg faulting but inserting first

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -10,8 +10,44 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-	*root = *root;
-	value = value;
+	heap_t *current = *root;
+	heap_t *new = malloc(sizeof(heap_t));
+		if (new == NULL)
+			return (NULL);
 
-	return (*root);
+	if (*root == NULL)
+	{
+		new->n = value;
+		new->parent = new->left = new->right = NULL;
+		*root = new;
+		return (new);
+	}
+	else
+	{
+		if (current->left == NULL)
+		{
+			current->left = new;
+			new->n = value;
+			new->parent = current;
+			new->left = new->right = NULL;
+
+			if (value > current->n)
+			{
+				current = new;
+				return (new);
+			}
+		}
+		else
+		{
+			current->right = new;
+			new->n = value;
+			new->parent = current;
+			if (value > current->n)
+			{
+				new = current;
+				return (new);
+			}
+		}
+	}
+	return (NULL);
 }
