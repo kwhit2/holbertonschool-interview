011bb64c5171bcd43f1b8faf072d28b736c51e1d
kwhit2 <kfw2@outlook.com>
2021-08-10 12:50:28 +0000

    Upload

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Sum of prime factors """
+
+
+def prime_factors(n):
+    """ Brute force the return of the prime factors of n in a list """
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+def minOperations(n):
+    """ deterine minumum number of operations """
+    if n == 0 or n == 1:
+        return 0
+    else:
+        factList = prime_factors(n)
+        return sum(factList)
