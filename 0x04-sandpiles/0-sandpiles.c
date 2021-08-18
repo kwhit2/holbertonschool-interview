fb4a68013882a8708d01310c749cddc9bcd373ce
kwhit2 <kfw2@outlook.com>
2021-08-18 16:45:02 +0000

    Adding functions but still not toppling

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,4 +1,5 @@
 #include "sandpiles.h"
+
 /**
 * sandpiles_sum - Function that computes the sum of two sandpiles
 * @grid1: sandpile1
@@ -7,13 +8,70 @@
 */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+		}
+	}
+}
+
+/**
+* topple - Function to topple a sandpile
+* @grid1: sandpile
+* Return: void
+*/
+void topple(int grid1[3][3])
+{
+	int i, j, num;
+	int nextgrid[3][3];
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			num = grid1[i][j];
+			if (num < 4)
+			{
+				nextgrid[i][j] = grid1[i][j];
+				print_gridd(grid1);
+
+			}
+
+			if (num >= 4)
+			{
+				nextgrid[i][j] = grid1[i][j];
+				nextgrid[i + 1][j]++;
+				nextgrid[i - 1][j]++;
+				nextgrid[i][j + 1]++;
+				nextgrid[i][j - 1]++;
+				print_gridd(grid1);
+			}
+		}
+	}
+	grid1 = nextgrid;
+}
+
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ * Return: void
+ */
+void print_gridd(int grid[3][3])
+{
     int i, j;
 
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            grid1[i][j] = grid1[i][j] + grid2[i][j];
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
         }
+        printf("\n");
     }
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,5 +5,7 @@
 #include <string.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_gridd(int grid[3][3]);
+void topple(int grid1[3][3]);
 
 #endif
