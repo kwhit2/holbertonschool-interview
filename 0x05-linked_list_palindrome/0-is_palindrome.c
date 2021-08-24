696b25b6287dc1d76725640c0ab63251565e3a7f
Kyle Whitten <kfw2@outlook.com>
2021-08-24 07:48:57 -0500

    Project Bones

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: (int) 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+ int is_palindrome(listint_t **head)
+ {
+     
+ }
