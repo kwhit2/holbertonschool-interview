a70b31078523da46660d7e05fd83b95db7b16784
kwhit2 <kfw2@outlook.com>
2021-08-24 14:04:50 +0000

    Compiling with output

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 /**
  * struct listint_s - singly linked list
@@ -20,5 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int pal_meat(listint_t **left, listint_t *right);
 
 #endif /* LISTS_H */
