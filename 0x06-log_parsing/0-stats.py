c5819c9f27665a702194448f6c4c977bacc0ef72
kwhit2 <kfw2@outlook.com>
2021-08-30 16:57:05 +0000

    Still needs improvement

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+count = 0
+fileSizeCt = 0
+
+for line in sys.stdin:
+    try:
+        metricsInput = line.rstrip()
+        count += 1
+        fileSize = metricsInput[-3:]
+        fileSizeCt += 1
+        statusCode = metricsInput
+        # fileSize = 
+        if (count % 10 == 0):
+            print("{}: ".format(fileSize))
+            print("{}".format(metricsInput))
+    except KeyboardInterrupt: 
+        print('Hello user you have pressed ctrl-c button.')
