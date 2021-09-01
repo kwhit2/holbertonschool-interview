b6e2aa4c7ee3df83fb0b73ae49584e92a2dc05f3
kwhit2 <kfw2@outlook.com>
2021-09-01 16:44:34 +0000

    Output looking good now!

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
 """ script that reads stdin line by line and computes metrics """
-import sys
+from sys import stdin
 
-count = 0
-fileSizeCt = 0
 
-for line in sys.stdin:
-    try:
-        metricsInput = line.rstrip()
-        count += 1
-        fileSize = metricsInput[-3:]
-        fileSizeCt += 1
-        statusCode = metricsInput
-        # fileSize =
-        if (count % 10 == 0):
-            print("{}: ".format(fileSize))
-            print("{}".format(metricsInput))
-    except KeyboardInterrupt:
-        print('Hello user you have pressed ctrl-c button.')
+def printMetrics(statusCodes, fileSize):
+    """ ^ print metrics ^ """
+    print("File Size: " + str(fileSize))
+    for line in sorted(statusCodes.keys()):
+        if statusCodes[line] > 0:
+            print(line + ": " + str(statusCodes[line]))
+
+
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+statusCode = 0
+lineCount = 0
+fileSize = 0
+
+
+try:
+    for line in stdin:
+        lineCount += 1
+        splitArgs = line.split()
+
+        if len(splitArgs) > 1:
+            fileSize += int(splitArgs[-1])
+
+        if len(splitArgs) > 2 and splitArgs[-2].isnumeric():
+            statusCode = splitArgs[-2]
+        else:
+            statusCode = 0
+
+        if statusCode in statusCodes.keys():
+            statusCodes[statusCode] += 1
+
+        if lineCount % 10 == 0:
+            printMetrics(statusCodes, fileSize)
+
+    printMetrics(statusCodes, fileSize)
+
+except KeyboardInterrupt:
+    printMetrics(statusCodes, fileSize)
