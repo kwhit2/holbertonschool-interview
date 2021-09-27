ba25b5821d5f9fc305f536a302e44549ef794911
kwhit2 <kfw2@outlook.com>
2021-09-27 13:14:57 +0000

    Bones Upload

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,15 @@
+#include "slide_line.h"
+
+/**
+* slide_line - The goal of this task is to reproduce the 2048...
+* ...game(NSFW !!) mechanics on a single horizontal line.
+* @line: points to an array of integers containing...
+* @size: elements that must be slide and merged to the direction
+* ...represented by...
+& @direction: direction to slide (SLIDE_LEFT & SLIDE_RIGHT)
+* Return: 1 for success, and 0 otherwise
+*/
+int slide_line(int *line, size_t size, int direction)
+{
+
+}
