85100d81468fb35c823f21b84651faf577b0db7e
kwhit2 <kfw2@outlook.com>
2021-10-04 14:55:27 +0000

    Bones Upload

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,11 @@
+#include "menger.h"
+
+/**
+* menger - a function that draws a 2D Menger Sponge
+* @level: level of the Menger Sponge to draw
+* Return: int / draw output of 2d menger sponge
+*/
+void menger(int level)
+{
+
+}
