8c05eea410fe3182df3374f4d0934fe85db4f14c
kwhit2 <kfw2@outlook.com>
2021-10-06 16:10:25 +0000

    Removed most everything, could not get rid of trash output along with the desire output

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -5,22 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
-* struct sCarpet - doubly linked list
-* @dim: dimensions
-* @data: character data
-* @rows: number of rows
-*/
-typedef struct sCarpet
-{
-	int dim;
-	char *data;
-	char **rows;
-} *Carpet;
-
 void menger(int level);
-void TileCarpet(Carpet d, int r, int c, Carpet tile);
-Carpet Sierpinski(int level);
-void CarpetPrint(Carpet carp);
 
 #endif
