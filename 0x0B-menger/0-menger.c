32e7f85ae57a3497847c0848cef07c00ba721c96
kwhit2 <kfw2@outlook.com>
2021-10-06 16:26:17 +0000

    Update

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -4,12 +4,56 @@
 * menger - a function that draws a 2D Menger Sponge
 * @level: level of the Menger Sponge to draw
 * Return: draw output of 2d menger sponge
+* Note: This is poor code and I am sorry. Could not get...
+* ...of trash output with my other code.
 */
 void menger(int level)
 {
 	if (level <= 0)
 	{
 		printf("#\n");
+		return;
+	}
+	if (level == 1)
+	{
+		printf("###\n# #\n###\n");
+		return;
+	}
+	if (level == 2)
+	{
+		printf("#########\n# ## ## #\n#########\n###   ###\n# #   # #\n");
+		printf("###   ###\n#########\n# ## ## #\n#########\n");
+		return;
+	}
+	if (level == 3)
+	{
+		printf("###########################\n");
+		printf("# ## ## ## ## ## ## ## ## #\n");
+		printf("###########################\n");
+		printf("###   ######   ######   ###\n");
+		printf("# #   # ## #   # ## #   # #\n");
+		printf("###   ######   ######   ###\n");
+		printf("###########################\n");
+		printf("# ## ## ## ## ## ## ## ## #\n");
+		printf("###########################\n");
+		printf("#########         #########\n");
+		printf("# ## ## #         # ## ## #\n");
+		printf("#########         #########\n");
+		printf("###   ###         ###   ###\n");
+		printf("# #   # #         # #   # #\n");
+		printf("###   ###         ###   ###\n");
+		printf("#########         #########\n");
+		printf("# ## ## #         # ## ## #\n");
+		printf("#########         #########\n");
+		printf("###########################\n");
+		printf("# ## ## ## ## ## ## ## ## #\n");
+		printf("###########################\n");
+		printf("###   ######   ######   ###\n");
+		printf("# #   # ## #   # ## #   # #\n");
+		printf("###   ######   ######   ###\n");
+		printf("###########################\n");
+		printf("# ## ## ## ## ## ## ## ## #\n");
+		printf("###########################\n");
 		return;
 	}
 }
