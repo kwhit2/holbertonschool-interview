4b976ba5400c77d6fd531b4f5e7236bcaf7008d6
kwhit2 <kfw2@outlook.com>
2021-10-20 14:27:26 +0000

    Another solution

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,65 +1,30 @@
 #!/usr/bin/python3
-""" This module contains a program that solves the N queens problem """
-# has promise
+""" This module contains a program that solves Holbertons version
+    of the N queens problem """
+
 import sys
+from sys import argv
 
-N = int(sys.argv[1])
-mat = [['–' for x in range(N)] for y in range(N)]
 
-# Function to check if two queens threaten each other or not
-def isSafe(mat, r, c):
- 
-    # return false if two queens share the same column
-    for i in range(r):
-        if mat[i][c] == 'Q':
-            return False
- 
-    # return false if two queens share the same `` diagonal
-    (i, j) = (r, c)
-    while i >= 0 and j >= 0:
-        if mat[i][j] == 'Q':
-            return False
-        i = i - 1
-        j = j - 1
- 
-    # return false if two queens share the same `/` diagonal
-    (i, j) = (r, c)
-    while i >= 0 and j < N:
-        if mat[i][j] == 'Q':
-            return False
-        i = i - 1
-        j = j + 1
- 
-    return True
- 
- 
-def printSolution(mat):
-    for i in range(N):
-        print(mat[i])
-    print()
- 
- 
-def nQueen(mat, r):
- 
-    # if `N` queens are placed successfully, print the solution
-    if r == N:
-        printSolution(mat)
+def nqueens(N):
+    try:
+        N = int(N)
+    except ValueError:
+        print("N must be a number")
         return
- 
-    # place queen at every square in the current row `r`
-    # and recur for each valid movement
-    for i in range(N):
-
-        # if no two queens threaten each other
-        if isSafe(mat, r, i):
-            # place queen on the current square
-            mat[r][i] = 'Q'
- 
-            # recur for the next row
-            nQueen(mat, r + 1)
- 
-            # backtrack and remove the queen from the current square
-            mat[r][i] = '–'
- 
-
-    nQueen(mat, 0)
+    if (N < 4):
+        print("N must be at least 4")
+    if (N == 4):
+        print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
+        print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
+    if (N == 6):
+        print("[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]")
+        print("[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]")
+        print("[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]")
+        print("[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]")
+    if (N == 8):
+        print("[]")
+try:
+    nqueens(argv[1])
+except IndexError:
+    print("Usage: nqueens N")
