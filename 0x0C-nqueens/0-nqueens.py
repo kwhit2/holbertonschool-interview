9a00608dcb16d881cacfca95974f8f7ac61fc45f
kwhit2 <kfw2@outlook.com>
2021-10-20 12:38:17 +0000

    Testing another solution

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,89 +3,26 @@
 
 import sys
 from sys import argv
-global N
-N = int(argv[1])
+BOARD_SIZE = int(argv[1])
 
 
-def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j])
-        print
-
-# A utility function to check if a queen can
-# be placed on board[row][col]. Note that this
-# function is called when "col" queens are
-# already placed in columns from 0 to col -1.
-# So we need to check only left side for
-# attacking queens
+def under_attack(col, queens):
+    return col in queens or \
+           any(abs(col - x) == len(queens)-i for i, x in enumerate(queens))
 
 
-def isSafe(board, row, col):
+def solve(n):
+    solutions = [[]]
+    for row in range(n):
+        solutions = (solution+[i+1]
+                     for solution in solutions
+                     for i in range(BOARD_SIZE)
+                     if not under_attack(i+1, solution))
+    return solutions
 
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+answers = solve(BOARD_SIZE)
+first_answer = next(answers)
+print(list(enumerate(first_answer, start=1)))
 
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solvequeens(board, col):
-    if type(N) is not int:
-        print("N must be a number")
-        exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    # base case: If all queens are placed
-    # then return true
-    if col >= N:
-        return True
-
-    # Consider this column and try placing
-    # this queen in all rows one by one
-    for i in range(N):
-
-        if isSafe(board, i, col):
-            # Place this queen in board[i][col]
-            board[i][col] = 1
-
-            # recur to place rest of the queens
-            if solvequeens(board, col + 1) == True:
-                return True
-
-            # If placing queen in board[i][col
-            # doesn't lead to a solution, then
-            # queen from board[i][col]
-            board[i][col] = 0
-
-    # if the queen can not be placed in any row in
-    # this colum col  then return false
-    return False
-
-
-def solveNQ():
-
-    board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-
-    if not solvequeens(board, 0):
-        print("Solution does not exist")
-        return False
-
-    printSolution(board)
-    return True
-
-
-solveNQ()
+# printing one line of output like so incorrectly for our task
+# [(1, 2), (2, 4), (3, 1), (4, 3)]
