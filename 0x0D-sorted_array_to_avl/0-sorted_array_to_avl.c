6e4ea9a9030bf8248e6ae4e15938f0d0b6085c0f
Kyle Whitten <kfw2@outlook.com>
2021-10-25 10:12:22 -0500

    Update

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -19,3 +19,33 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
     }
     return (root);
 }
+avl_t avl_insert(avl_t *root, int value)
+{
+    avl_t *new_node = NULL;
+    avl_t *temp = NULL;
+    avl_t *parent = NULL;
+
+    new_node = malloc(sizeof(avl_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->height = 0;
+    if (root == NULL)
+        return (new_node);
+    temp = root;
+    while (temp != NULL)
+    {
+        parent = temp;
+        if (value < temp->n)
+            temp = temp->left;
+        else
+            temp = temp->right;
+    }
+    if (value < parent->n)
+        parent->left = new_node;
+    else
+        parent->right = new_node;
+    return (new_node);
+}
