cb02811dae6e9abedf54df1e2cdf700cebfacdd4
kwhit2 <kfw2@outlook.com>
2021-10-27 17:34:01 +0000

    Betty

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,16 +8,16 @@
 */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *root = NULL;
-    size_t i;
+	avl_t *root = NULL;
+	size_t i;
 
-    if (array == NULL)
-        return (NULL);
-    for (i = 0; i < size; i++)
-    {
-        root = avl_insert(root, array[i]);
-    }
-    return (root);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		root = avl_insert(root, array[i]);
+	}
+	return (root);
 }
 
 /**
@@ -28,32 +28,32 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
  */
 avl_t *avl_insert (avl_t *root, int value)
 {
-    avl_t *new_node, *parent, *current;
+	avl_t *new_node, *parent, *current;
 
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->parent = NULL;
-    /*new_node->height = 0;*/
-    if (root == NULL)
-        return (new_node);
-    parent = NULL;
-    current = root;
-    while (current != NULL)
-    {
-        parent = current;
-        if (value < current->n)
-            current = current->left;
-        else
-            current = current->right;
-    }
-    if (value < parent->n)
-        parent->left = new_node;
-    else
-        parent->right = new_node;
-    new_node->parent = parent;
-    return (new_node);
+	new_node = malloc(sizeof(avl_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = NULL;
+	/*new_node->height = 0;*/
+	if (root == NULL)
+		return (new_node);
+	parent = NULL;
+	current = root;
+	while (current != NULL)
+	{
+		parent = current;
+		if (value < current->n)
+			current = current->left;
+		else
+			current = current->right;
+	}
+	if (value < parent->n)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+	new_node->parent = parent;
+	return (new_node);
 }
