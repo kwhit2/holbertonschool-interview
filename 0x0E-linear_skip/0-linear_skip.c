11f9e89e7de489a1fd6ae510d908a0b316f95a69
kwhit2 <kfw2@outlook.com>
2021-11-01 14:25:03 +0000

    Bones Upload

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,15 @@
+#include "search.h"
+
+/**
+* *linear_skip - function that searches for a value in a sorted... 
+* ...skip list of integers.
+* @list: a pointer to the head of the skip list to search in
+* @value: value to search for
+* Return: a pointer on the first node where value is located...
+* ...If value is not present in list or if head is NULL, your...
+* ...function must return NULL
+*/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+}
