24818ad2609c27f7d69e483a5820b57b47aa46b0
Kyle Whitten <kfw2@outlook.com>
2021-11-01 15:31:45 -0500

    Return comment update

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -6,8 +6,7 @@
 * @list: a pointer to the head of the skip list to search in
 * @value: value to search for
 * Return: a pointer on the first node where value is located...
-* ...If value is not present in list or if head is NULL, your...
-* ...function must return NULL
+* ...If value is not present in list or if head is NULL, return NULL
 */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
