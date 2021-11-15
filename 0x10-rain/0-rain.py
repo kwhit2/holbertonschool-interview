c6c4607fab5af86abcc92e222ae33c6e9becf0fa
kwhit2 <kfw2@outlook.com>
2021-11-15 15:50:23 +0000

    Bones Upload

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" calculate how many square units of water will be retained after rain
+    walls is a list of non-negative integers
+    Return: Integer indicating total amount of rainwater retained
+    If the list is empty return 0 """
+
+
+def rain(walls)
