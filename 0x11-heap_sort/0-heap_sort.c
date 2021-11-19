288877e051489ecc5dc1e3fc9f351e1f3b1a2578
kwhit2 <kfw2@outlook.com>
2021-11-19 16:38:55 +0000

    Compiling but output if off. Preliminary answer files in 0-O

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -7,8 +7,74 @@
 * @size: size of the array
 * Return: void
 */
-
 void heap_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
+	helper_heap_sort(array, size, array, size);
+}
 
+/**
+* helper_heap_sort - function that helps & prints
+* @array: ints
+* @size: size of the array
+* @arrcpy: copy of entire array for printing
+* @cpysize: copy of size for printing
+* Return: void
+*/
+void helper_heap_sort(int *array, size_t size, int *arrcpy, size_t cpysize)
+{
+	size_t i = 0, temp = 0;
+
+	for (i = (size - 1) / 2; (int)i >= 0; i--)
+	{
+		sift_down(array, i, size, arrcpy, cpysize);
+	}
+
+	for (i = size - 1; i > 0; i--)
+	{
+		temp = array[i];
+		array[i] = array[0];
+		array[0] = temp;
+		arrcpy[i] = array[i];
+		arrcpy[0] = array[0];
+		print_array(array, size);
+		sift_down(array, 0, i, arrcpy, cpysize);
+	}
+}
+
+/**
+* sift_down - function that sifts down
+* @arr: ints
+* @i: index
+* @size: size of array
+* @arrcpy: copy of entire array for printing
+* @cpysize: copy of size of array for printing
+* Return: void
+*/
+void sift_down(int *arr, size_t i, size_t size, int *arrcpy, size_t cpysize)
+{
+	size_t left, right, largest;
+	int temp = 0;
+
+	left = 2 * i + 1;
+	right = 2 * i + 2;
+	largest = i;
+
+	if (left < size && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < size && arr[right] > arr[largest])
+		largest = right;
+
+	if (largest != i)
+	{
+		temp = arr[i];
+		arr[i] = arr[largest];
+		arr[largest] = temp;
+		arrcpy[i] = arr[i];
+		arrcpy[largest] = arr[largest];
+		print_array(arr, size);
+		sift_down(arr, largest, size, arrcpy, cpysize);
+	}
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void helper_heap_sort(int *array, size_t size, int *cpyarray, size_t cpysize);
+void sift_down(int *arr, size_t i, size_t size, int *cpyarray, size_t cpysize);
+void swap(int *array, int index1, int index2);
 
 #endif
