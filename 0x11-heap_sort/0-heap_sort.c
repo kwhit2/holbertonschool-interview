40f458f9fd15991b90a9234b66457b6103770475
kwhit2 <kfw2@outlook.com>
2021-11-22 15:31:01 +0000

    Changed some variables to int from size_t and reworked indexing logic

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -9,7 +9,7 @@
 */
 void heap_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (array == NULL || size <= 1)
 		return;
 	helper_heap_sort(array, size, array, size);
 }
@@ -24,14 +24,14 @@ void heap_sort(int *array, size_t size)
 */
 void helper_heap_sort(int *array, size_t size, int *arrcpy, size_t cpysize)
 {
-	size_t i = 0, temp = 0;
+	int i = 0, temp = 0;
 
-	for (i = (size - 1) / 2; (int)i >= 0; i--)
+	for (i = (size / 2) - 1; i >= 0; i--)
 	{
-		sift_down(array, i, size, arrcpy, cpysize);
+		sift_down(array, size, i, arrcpy, cpysize);
 	}
 
-	for (i = size - 1; i > 0; i--)
+	for (i = (size - 1); i > 0; i--)
 	{
 		temp = array[i];
 		array[i] = array[0];
@@ -39,7 +39,7 @@ void helper_heap_sort(int *array, size_t size, int *arrcpy, size_t cpysize)
 		arrcpy[i] = array[i];
 		arrcpy[0] = array[0];
 		print_array(array, size);
-		sift_down(array, 0, i, arrcpy, cpysize);
+		sift_down(array, i, 0, arrcpy, cpysize);
 	}
 }
 
@@ -52,19 +52,19 @@ void helper_heap_sort(int *array, size_t size, int *arrcpy, size_t cpysize)
 * @cpysize: copy of size of array for printing
 * Return: void
 */
-void sift_down(int *arr, size_t i, size_t size, int *arrcpy, size_t cpysize)
+void sift_down(int *arr, size_t size, int i, int *arrcpy, size_t cpysize)
 {
-	size_t left, right, chonky;
+	int left, right, chonky;
 	int temp = 0;
 
 	left = 2 * i + 1;
 	right = 2 * i + 2;
 	chonky = i;
 
-	if (left < size && arr[left] > arr[chonky])
+	if (left < (int)size && arr[left] > arr[chonky])
 		chonky = left;
 
-	if (right < size && arr[right] > arr[chonky])
+	if (right < (int)size && arr[right] > arr[chonky])
 		chonky = right;
 
 	if (chonky != i)
@@ -75,6 +75,6 @@ void sift_down(int *arr, size_t i, size_t size, int *arrcpy, size_t cpysize)
 		arrcpy[i] = arr[i];
 		arrcpy[chonky] = arr[chonky];
 		print_array(arr, size);
-		sift_down(arr, chonky, size, arrcpy, cpysize);
+		sift_down(arr, size, chonky, arrcpy, cpysize);
 	}
 }
