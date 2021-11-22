f0e6fba707bb06c2e3e04ddf19724a8ce8ec25cd
kwhit2 <kfw2@outlook.com>
2021-11-22 15:56:39 +0000

    Removed a function to make another recursive and added to the original function. Improved Output.

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -9,62 +9,51 @@
 */
 void heap_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1)
-		return;
-	helper_heap_sort(array, size, array, size);
-}
+	int i, temp;
 
-/**
-* helper_heap_sort - function that helps & prints
-* @array: ints
-* @size: size of the array
-* @arrcpy: copy of entire array for printing
-* @cpysize: copy of size for printing
-* Return: void
-*/
-void helper_heap_sort(int *array, size_t size, int *arrcpy, size_t cpysize)
-{
-	int i = 0, temp = 0;
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = (size / 2) - 1; i >= 0; i--)
 	{
-		sift_down(array, size, i, arrcpy, cpysize);
+		sift_down(array, size, i, size);
 	}
 
 	for (i = (size - 1); i > 0; i--)
 	{
-		temp = array[i];
-		array[i] = array[0];
-		array[0] = temp;
-		arrcpy[i] = array[i];
-		arrcpy[0] = array[0];
-		print_array(array, size);
-		sift_down(array, i, 0, arrcpy, cpysize);
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+
+		if (i != 0)
+		{
+			print_array(array, size);
+		}
+		sift_down(array, i, 0, size);
 	}
 }
 
 /**
-* sift_down - function that sifts down
+* sift_down - function that sifts down values
 * @arr: ints
-* @i: index
 * @size: size of array
-* @arrcpy: copy of entire array for printing
+* @i: index
 * @cpysize: copy of size of array for printing
 * Return: void
 */
-void sift_down(int *arr, size_t size, int i, int *arrcpy, size_t cpysize)
+void sift_down(int *arr, size_t size, size_t i, size_t cpysize)
 {
-	int left, right, chonky;
+	size_t left, right, chonky;
 	int temp = 0;
 
 	left = 2 * i + 1;
 	right = 2 * i + 2;
 	chonky = i;
 
-	if (left < (int)size && arr[left] > arr[chonky])
+	if (left < size && arr[left] > arr[chonky])
 		chonky = left;
 
-	if (right < (int)size && arr[right] > arr[chonky])
+	if (right < size && arr[right] > arr[chonky])
 		chonky = right;
 
 	if (chonky != i)
@@ -72,9 +61,7 @@ void sift_down(int *arr, size_t size, int i, int *arrcpy, size_t cpysize)
 		temp = arr[i];
 		arr[i] = arr[chonky];
 		arr[chonky] = temp;
-		arrcpy[i] = arr[i];
-		arrcpy[chonky] = arr[chonky];
-		print_array(arr, size);
-		sift_down(arr, size, chonky, arrcpy, cpysize);
+		print_array(arr, cpysize);
+		sift_down(arr, size, chonky, cpysize);
 	}
 }
