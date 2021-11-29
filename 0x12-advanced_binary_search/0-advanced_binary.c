2900d73e48b17d52d946ab31dcfa84c6d3fa5277
kwhit2 <kfw2@outlook.com>
2021-11-29 13:44:11 +0000

    Bones Upload

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+* advanced_binary - a function that searches for a value in a sorted...
+* ...array of integers
+* order using the (sift down) Heap sort algorithm
+* @array: is a pointer to the first element of the array to search in
+* @size: number of elements in the array
+* @value: is the value to be searched for
+* Return: the index where value is located
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
