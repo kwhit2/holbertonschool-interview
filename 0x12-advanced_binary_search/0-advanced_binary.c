59e8d97c212f293085ce41226dbc046c0e56227e
kwhit2 <kfw2@outlook.com>
2021-12-01 14:13:06 +0000

    Null check fix to correct output

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -15,7 +15,7 @@ int advanced_binary(int *array, size_t size, int value)
 	int outcome = 0;
 	size_t i = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size < 1)
 		return (-1);
 
 	print_searching_output(&array[i], size);
