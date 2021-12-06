d87cdea7a7e4ae8e253abd12569a9a844dc1ab39
kwhit2 <kfw2@outlook.com>
2021-12-06 17:52:18 +0000

    Added another solution but it is not compiling yet

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -8,20 +8,31 @@ import requests
 from sys import argv
 
 
-def count_words(subreddit, word_list):
-    """ recursive function that queries the Reddit API, parses the title of
-        all hot articles, and prints a sorted count of given keywords
-        (case-insensitive, delimited by spaces. Javascript should count as
-        javascript, but java should not) """
+# * word causing problems
+def count_words(subreddit, word_list, count=0):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    r = requests.get(url, headers=headers)
-    if r.status_code != 200:
-        return None
-    data = r.json()
-    titles = []
+    header = {'User-Agent': 'test'}
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json()
     for post in data.get('data').get('children'):
-        titles.append(post.get('data').get('title'))
-    for word in word_list:
-        count = titles.count(word)
-        print('{}: {}'.format(word, count))
+        count += post.get('data').get('title').lower().split().count(word)
+    if data.get('data').get('after'):
+        count_words(subreddit, word_list, count)
+    return count
+
+# * Gives output but all count is 0
+# def count_words(subreddit, word_list):
+#     url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
+#     headers = {'User-Agent': 'test'}
+#     r = requests.get(url, headers=headers)
+#     if r.status_code != 200:
+#         return None
+#     data = r.json()
+#     titles = []
+#     for post in data.get('data').get('children'):
+#         titles.append(post.get('data').get('title'))
+#     for word in word_list:
+#         count = titles.count(word)
+#         print('{}: {}'.format(word, count))
