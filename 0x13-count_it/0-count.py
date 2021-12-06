d3a12c59d470207be580fde359fd300e097e7209
kwhit2 <kfw2@outlook.com>
2021-12-06 19:13:59 +0000

    Printing complete titles & count currently. Will improve

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -8,18 +8,28 @@ import requests
 from sys import argv
 
 
-# * word causing problems
-def count_words(subreddit, word_list, count=0):
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-    header = {'User-Agent': 'test'}
-    response = requests.get(url, headers=header, allow_redirects=False)
+def count_words(subreddit, word_list={}, count=0, after=None):
+    """ recursive function that queries the Reddit API, parses the title of all
+        hot articles, and prints a sorted count of given keywords
+        (case-insensitive, delimited by spaces. Javascript should count as
+        javascript, but java should not) """
+    headers = {'User-agent': 'i am a python program'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if after is not None:
+        url += '?after={}'.format(after)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        return 0
+        return None
     data = response.json()
     for post in data.get('data').get('children'):
-        count += post.get('data').get('title').lower().split().count(word)
-    if data.get('data').get('after'):
-        count_words(subreddit, word_list, count)
+        title = post.get('data').get('title')
+        for word in word_list:
+            if word.lower() in title.lower():
+                count += 1
+    after = data.get('data').get('after')
+    if after is not None:
+        return count_words(subreddit, word_list, count, after)
+    print('{}: {}'.format(word, count))
     return count
 
 # * Gives output but all count is 0
