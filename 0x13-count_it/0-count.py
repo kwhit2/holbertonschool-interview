c499576052f2f70f85c8d0389ef61755f0dd615b
kwhit2 <kfw2@outlook.com>
2021-12-08 14:14:18 +0000

    Checking other solution

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -8,41 +8,41 @@ import requests
 from sys import argv
 
 
-def count_words(subreddit, word_list={}, count=0, after=None):
-    """ recursive function that queries the Reddit API, parses the title of all
-        hot articles, and prints a sorted count of given keywords
-        (case-insensitive, delimited by spaces. Javascript should count as
-        javascript, but java should not) """
-    headers = {'User-agent': 'i am a python program'}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    if after is not None:
-        url += '?after={}'.format(after)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return None
-    data = response.json()
-    for post in data.get('data').get('children'):
-        title = post.get('data').get('title')
-        for word in word_list:
-            if word.lower() in title.lower():
-                count += 1
-    after = data.get('data').get('after')
-    if after is not None:
-        return count_words(subreddit, word_list, count, after)
-    print('{}: {}'.format(word, count))
-    return count
+# def count_words(subreddit, word_list={}, count=0, after=None):
+#     """ recursive function that queries the Reddit API, parses the title of all
+#         hot articles, and prints a sorted count of given keywords
+#         (case-insensitive, delimited by spaces. Javascript should count as
+#         javascript, but java should not) """
+#     headers = {'User-agent': 'i am a python program'}
+#     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+#     if after is not None:
+#         url += '?after={}'.format(after)
+#     response = requests.get(url, headers=headers, allow_redirects=False)
+#     if response.status_code != 200:
+#         return None
+#     data = response.json()
+#     for post in data.get('data').get('children'):
+#         title = post.get('data').get('title')
+#         for word in word_list:
+#             if word.lower() in title.lower():
+#                 count += 1
+#     after = data.get('data').get('after')
+#     if after is not None:
+#         return count_words(subreddit, word_list, count, after)
+#     print('{}: {}'.format(word, count))
+#     return count
 
 # * Gives output but all count is 0
-# def count_words(subreddit, word_list):
-#     url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-#     headers = {'User-Agent': 'test'}
-#     r = requests.get(url, headers=headers)
-#     if r.status_code != 200:
-#         return None
-#     data = r.json()
-#     titles = []
-#     for post in data.get('data').get('children'):
-#         titles.append(post.get('data').get('title'))
-#     for word in word_list:
-#         count = titles.count(word)
-#         print('{}: {}'.format(word, count))
+def count_words(subreddit, word_list):
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
+    headers = {'User-Agent': 'test'}
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        return None
+    data = r.json()
+    titles = []
+    for post in data.get('data').get('children'):
+        titles.append(post.get('data').get('title'))
+    for word in word_list:
+        count = titles.count(word)
+        print('{}: {}'.format(word, count))
