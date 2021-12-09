eb884d8244a79892d47823e6619a5eb2dc30410d
kwhit2 <kfw2@outlook.com>
2021-12-09 15:54:53 +0000

    Implemented orderedDict

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,26 +3,43 @@
     hot articles, and prints a sorted count of given keywords
     (case-insensitive, delimited by spaces. Javascript should count as
     javascript, but java should not) """
+from collections import OrderedDict
 import json
 import requests
 from sys import argv
 
 
-def count_words(subreddit, word_list={}, count=0, after=None):
-    headers = {'User-agent': 'i am a python program'}
+def count_words(subreddit, word_list, word_dict={}, count=0, after=None):
+    word_dict = OrderedDict(word_dict)
+    headers = {'User-agent': 'test'}
+    parameter = {'limit': 100, 'after': after}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    if after is not None:
-        url += '?after={}'.format(after)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        response = requests.get(url, headers=headers, params=parameter,
+                                allow_redirects=False)
+    except BaseException:
+        return None
     if response.status_code != 200:
         return None
-    data = response.json()
-    for post in data.get('data').get('children'):
-        title = post.get('data').get('title')
-        for word in word_list:
-            if word.lower() in title.lower():
-                count += 1
-    after = data.get('data').get('after')
+    try:
+        data = response.json()
+        rspn = data.get('data')
+        child = rspn.get('children')
+        after = rspn.get('after')
+    except BaseException:
+        return None
+    for titles in child:
+        postTitle = (titles.get('data')['title']).lower()
+        for post in postTitle.split():
+            for word in word_list:
+                word = word.lower()
+                if post == word:
+                    if word in word_dict.keys():
+                        word_dict[word] += 1
+                    else:
+                        word_dict[word] = 1
     if after is not None:
-        return count_words(subreddit, word_list, count, after)
-    return count
+        return count_words(subreddit, word_list, word_dict, count, after)
+    else:
+        for i in sorted(word_dict, key=word_dict.get, reverse=True):
+            print('{}: {}'.format(i, word_dict[i]))
