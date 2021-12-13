239d1eac7bdb7b58c97b2a609a1d8d398a6420b3
kwhit2 <kfw2@outlook.com>
2021-12-13 15:03:20 +0000

    Bones Upload

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - a function that extracts the root node of a Max Binary Heap
+* @root: is a double pointer to the root node of the heap
+* Return: the value stored at each node, if failed return 0
+*/
+int heap_extract(heap_t **root)
+{
+
+}
