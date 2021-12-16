cd3305aec30671de99e506feea5966f9c2db14bc
kwhit2 <kfw2@outlook.com>
2021-12-16 15:52:32 +0000

    Seg Fault

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -7,5 +7,47 @@
 */
 int heap_extract(heap_t **root)
 {
+	heap_t *tmp;
+	int value;
 
+	if (!root || !*root)
+		return (0);
+	tmp = *root;
+	value = tmp->n;
+	*root = tmp->left;
+	if (*root)
+		(*root)->parent = NULL;
+	free(tmp);
+	if (*root)
+		heap_sift_down(*root);
+	return (value);
+}
+
+/**
+* heap_sift_down - helper function
+* @root: is a double pointer to the root node of the heap
+* Return: void
+*/
+void heap_sift_down(heap_t *root)
+{
+	heap_t *tmp;
+
+	if (!root)
+		return;
+	while (root->left)
+	{
+		if (root->n > root->left->n)
+		{
+			if (root->n > root->right->n)
+			{
+				tmp = root->left;
+				root->left = root->right;
+				root->right = tmp;
+			}
+			tmp = root->left;
+			root->left = root->right;
+			root->right = tmp;
+		}
+		root = root->left;
+	}
 }
--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -26,5 +26,6 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
 int heap_extract(heap_t **root);
+void heap_sift_down(heap_t *root);
 
 #endif
