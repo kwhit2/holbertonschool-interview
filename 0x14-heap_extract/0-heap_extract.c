4d68aa8bd1fc495c9b2d2562a0b4762767035f35
Kyle Whitten <kfw2@outlook.com>
2021-12-20 09:05:24 -0600

    Update

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -7,45 +7,45 @@
 */
 int heap_extract(heap_t **root)
 {
-    heap_t *min = *root, *tmp = NULL;
-    int res;
+	heap_t *min = *root, *tmp = NULL;
+	int res;
 
-    if (!root || !(*root))
-        return (0);
-    res = (*root)->n;
+	if (!root || !(*root))
+		return (0);
+	res = (*root)->n;
 
-    if (!(*root)->left && !(*root)->right)
-    {
-        free(*root);
-        *root = NULL;
-        return (res);
-    }
-    while (min != NULL)
-    {
-        if (!min->left && !min->right)
-            break;
-        if (balance(min) <= 0)
-            min = min->right;
-        else
-            min = min->left;
-    }
-    if (!min->parent)
-    {
-        res = min->n;
-        free(min);
-        return (res);
-    }
-    tmp = min->parent;
-    (*root)->n = min->n;
-    if (tmp->left == min)
-        tmp->left = NULL;
-    else
-        tmp->right = NULL;
-    free(min);
+	if (!(*root)->left && !(*root)->right)
+	{
+		free(*root);
+		*root = NULL;
+		return (res);
+	}
+	while (min != NULL)
+	{
+		if (!min->left && !min->right)
+			break;
+		if (balance(min) <= 0)
+			min = min->right;
+		else
+			min = min->left;
+	}
+	if (!min->parent)
+	{
+		res = min->n;
+		free(min);
+		return (res);
+	}
+	tmp = min->parent;
+	(*root)->n = min->n;
+	if (tmp->left == min)
+		tmp->left = NULL;
+	else
+		tmp->right = NULL;
+	free(min);
 
-    tree_sort(*root);
+	tree_sort(*root);
 
-    return (res);
+	return (res);
 }
 
 /**
@@ -55,16 +55,16 @@ int heap_extract(heap_t **root)
  */
 size_t height(const binary_tree_t *tree)
 {
-    size_t left, right;
+	size_t left, right;
 
-    if (tree == NULL)
-        return (0);
-    left = height(tree->left);
-    right = height(tree->right);
-    if (left > right)
-    return (left + 1);
-    else
-    return (right + 1);
+	if (tree == NULL)
+		return (0);
+	left = height(tree->left);
+	right = height(tree->right);
+	if (left > right)
+	return (left + 1);
+	else
+	return (right + 1);
 }
 
 /**
@@ -74,24 +74,24 @@ size_t height(const binary_tree_t *tree)
  */
 void tree_sort(binary_tree_t *root)
 {
-    int temp_int;
-    heap_t *temp;
+	int temp_int;
+	heap_t *temp;
 
-    if (!root->left && !root->right)
-        return;
+	if (!root->left && !root->right)
+		return;
 
-    if (root->right && root->right->n > root->left->n)
-        temp = root->right;
-    else
-        temp = root->left;
+	if (root->right && root->right->n > root->left->n)
+		temp = root->right;
+	else
+		temp = root->left;
 
-    if (root->n < temp->n)
-    {
-        temp_int = root->n;
-        root->n = temp->n;
-        temp->n = temp_int;
-        tree_sort(temp);
-    }
+	if (root->n < temp->n)
+	{
+		temp_int = root->n;
+		root->n = temp->n;
+		temp->n = temp_int;
+		tree_sort(temp);
+	}
 }
 
 /**
@@ -101,15 +101,15 @@ void tree_sort(binary_tree_t *root)
  */
 int balance(const binary_tree_t *tree)
 {
-    int left, right, bal;
+	int left, right, bal;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    left = height(tree->left);
-    right = height(tree->right);
-    bal = left - right;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left = height(tree->left);
+	right = height(tree->right);
+	bal = left - right;
 
-    return (bal);
+	return (bal);
 }
