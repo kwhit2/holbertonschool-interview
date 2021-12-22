7c64f832587b2d182c701ded2c9d3e0924d180b4
Kyle Whitten <kfw2@outlook.com>
2021-12-22 14:19:24 -0600

    Update

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,13 +1,73 @@
-#include "mult.h"
+#include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int infinite_multiplication(int n)
+* ERR_output - error handling
+* @err: void
+* Return: 0
+*/
+
+void ERR_output(err)
 {
-	
+	char *e = "Error\n";
+	int i;
+
+	for (i = 0; i < 6; i++)
+		_putchar(e[i]);
+}
+
+/**
+* check_string - checks if string is int and if so, is it 0
+* @argv: input string
+* Return: -1 1 or 0
+*/
+int check_string(char *argv[])
+{
+	int i, j, z_check = 0;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				ERR_output();
+				return (-1);
+			}
+			if (argv[i][j] != '0')
+				z_check++;
+		}
+		if (z_check == 0)
+			return (0);
+		z_check = 0;
+	}
+	return (1);
+}
+
+/**
+* main - program that multiplies two positive numbers
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 98 if error, 0 if success
+*/
+int main(int argc, char *argv[])
+{
+	int str_check, ans = 0;
+	/*int a[100], b[100];*/
+
+	if (argc != 3)
+	{
+		ERR_output();
+		return (98);
+	}
+
+	str_check = check_string(argv);
+	if (str_check < 0)
+		return (98);
+	if (str_check == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+
+	return (ans);
 }
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void error_printing(void);
+int check_string(char *argv[]);
+int _putchar(char c);
+
+#endif
