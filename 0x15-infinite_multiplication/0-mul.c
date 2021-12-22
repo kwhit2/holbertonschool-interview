d5dbb8453862e2f4582d01ee547073a5b94d204a
Kyle Whitten <kfw2@outlook.com>
2021-12-22 13:45:32 -0600

    Bones upload

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,13 @@
+#include "mult.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int infinite_multiplication(int n)
+{
+	
+}
---
+++ 0x15-infinite_multiplication/mult.h
@@ -0,0 +1,11 @@
+#ifndef MULT_H
+#define MULT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
+int infinite_multiplication(int n);
+
+#endif
