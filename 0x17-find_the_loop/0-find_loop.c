09c94c43f10ed86b8cb90d16578be0fd2d6c82a3
kwhit2 <kfw2@outlook.com>
2022-01-10 18:04:18 +0000

    Compiling

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -2,8 +2,28 @@
 
 /**
 * find_listint_loop - a function that finds the loop in a linked list.
-* @head: head
-* Returns: The address of the node where the loop starts...
+* @head: pointer to the head of list
+* Return: The address of the node where the loop starts...
 * ...or NULL if there is no loop
 */
 listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
