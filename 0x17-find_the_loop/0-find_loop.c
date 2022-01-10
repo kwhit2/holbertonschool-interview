dda0327924c9579c37b56f5b2ba6cbdad1952e4e
kwhit2 <kfw2@outlook.com>
2022-01-10 14:57:48 +0000

    Bones Upload

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+* find_listint_loop - a function that finds the loop in a linked list.
+* @head: head
+* Returns: The address of the node where the loop starts...
+* ...or NULL if there is no loop
+*/
+listint_t *find_listint_loop(listint_t *head)
