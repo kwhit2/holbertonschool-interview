a55dcbb18151f9b2cb6303f880e28536f35d1613
kwhit2 <kfw2@outlook.com>
2022-01-17 17:21:23 +0000

    Compiling but missing output

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -8,43 +8,64 @@
 */
 void merge_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    merge_sort_rec(array, 0, size - 1);
+	merge_sort_rec(array, 0, size - 1);
 }
 
+/**
+* merge_sort_rec - Helper function for merge sort
+* @array: The array to be sorted
+* @start: The starting index of the array
+* @end: The ending index of the array
+* Return: Nothing
+*/
 void merge_sort_rec(int *array, size_t start, size_t end)
 {
-    size_t middle;
+	int *temp;
+	size_t mid;
 
-    if (start >= end)
-        return;
+	if (start >= end)
+		return;
 
-    middle = (start + end) / 2;
-    merge_sort_rec(array, start, middle);
-    merge_sort_rec(array, middle + 1, end);
-    merge(array, start, middle, end);
+	mid = (start + end) / 2;
+	merge_sort_rec(array, start, mid);
+	merge_sort_rec(array, mid + 1, end);
+	temp = malloc(sizeof(int) * (end - start + 1));
+	if (temp == NULL)
+		return;
+	merge(array, temp, start, mid, end);
+	free(temp);
 }
 
-void merge(int *array, size_t start, size_t middle, size_t end)
+/**
+* merge - Merges two sorted arrays
+* @array: The array to be sorted
+* @temp: Temporary array to hold sorted values
+* @start: The starting index of the array
+* @mid: The middle index of the array
+* @end: The ending index of the array
+* Return: Nothing
+*/
+void merge(int *array, int *temp, size_t start, size_t mid, size_t end)
 {
-    int *temp;
-    size_t i, j, k;
+	size_t i, j, k;
 
-    temp = malloc(sizeof(int) * (end - start + 1));
-    if (temp == NULL)
-        return;
-
-    i = start;
-    j = middle + 1;
-    k = 0;
-
-    while (i <= middle && j <= end)
-    {
-        if (array[i] < array[j])
-            temp[k++] = array[i++];
-        else
-            temp[k++] = array[j++];
-    }
+	i = start;
+	j = mid + 1;
+	k = 0;
+	while (i <= mid && j <= end)
+	{
+		if (array[i] < array[j])
+			temp[k++] = array[i++];
+		else
+			temp[k++] = array[j++];
+	}
+	while (i <= mid)
+		temp[k++] = array[i++];
+	while (j <= end)
+		temp[k++] = array[j++];
+	for (i = 0; i < k; i++)
+		array[start + i] = temp[i];
 }
--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -8,5 +8,7 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge_sort_rec(int *array, size_t start, size_t end);
+void merge(int *array, int *temp, size_t start, size_t mid, size_t end);
 
 #endif
