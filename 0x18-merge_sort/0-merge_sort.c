35175bd2ff77ca2aa4f822c65b9319d2a5e6f7fc
kwhit2 <kfw2@outlook.com>
2022-01-21 01:25:48 +0000

    Removed two functions, making it one function

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -8,64 +8,29 @@
 */
 void merge_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
-	merge_sort_rec(array, 0, size - 1);
-}
-
-/**
-* merge_sort_rec - Helper function for merge sort
-* @array: The array to be sorted
-* @start: The starting index of the array
-* @end: The ending index of the array
-* Return: Nothing
-*/
-void merge_sort_rec(int *array, size_t start, size_t end)
-{
-	int *temp;
-	size_t mid;
-
-	if (start >= end)
-		return;
-
-	mid = (start + end) / 2;
-	merge_sort_rec(array, start, mid);
-	merge_sort_rec(array, mid + 1, end);
-	temp = malloc(sizeof(int) * (end - start + 1));
-	if (temp == NULL)
-		return;
-	merge(array, temp, start, mid, end);
-	free(temp);
-}
-
-/**
-* merge - Merges two sorted arrays
-* @array: The array to be sorted
-* @temp: Temporary array to hold sorted values
-* @start: The starting index of the array
-* @mid: The middle index of the array
-* @end: The ending index of the array
-* Return: Nothing
-*/
-void merge(int *array, int *temp, size_t start, size_t mid, size_t end)
-{
 	size_t i, j, k;
+	int *tmp;
 
-	i = start;
-	j = mid + 1;
-	k = 0;
-	while (i <= mid && j <= end)
+	if (size < 2)
+		return;
+
+	tmp = malloc(sizeof(int) * size);
+	if (tmp == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		tmp[i] = array[i];
+
+	for (i = 0, j = size / 2, k = 0; k < size; k++)
 	{
-		if (array[i] < array[j])
-			temp[k++] = array[i++];
+		if (j >= size)
+			array[k] = tmp[i++];
+		else if (i >= size / 2)
+			array[k] = tmp[j++];
+		else if (tmp[i] < tmp[j])
+			array[k] = tmp[i++];
 		else
-			temp[k++] = array[j++];
+			array[k] = tmp[j++];
 	}
-	while (i <= mid)
-		temp[k++] = array[i++];
-	while (j <= end)
-		temp[k++] = array[j++];
-	for (i = 0; i < k; i++)
-		array[start + i] = temp[i];
+	free(tmp);
 }
--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -8,7 +8,5 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void merge_sort_rec(int *array, size_t start, size_t end);
-void merge(int *array, int *temp, size_t start, size_t mid, size_t end);
 
 #endif
