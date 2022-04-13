4039e79e2958dada072b5bdbacccc52dc46f50a4
Kyle Whitten <kfw2@outlook.com>
2022-04-13 11:30:53 -0500

    Upload

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" function def pascal_triangle(n): that returns a list of lists of integers
-representing the Pascal's triangle of n """
 
 
 def pascal_triangle(n):
+""" function def pascal_triangle(n): that returns a list of lists of integers
+representing the Pascal's triangle of n """
 	if n <= 0:
 		return []
 	if n == 1:
